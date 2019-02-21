247f20e510a7079bfb4731b93cfaeba50ef8b598
yaneeoke <yannick.leentvaar@gmail.com>
2019-02-21 08:54:04 +0100

    contact pagina

---
+++ surferdudes/surferdudes/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace surferdudes.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+
+        public string Voornaam { get; set; }
+
+        public string Achternaam { get; set; }
+
+        public string Telefoonnummer { get; set; }
+
+        public string Land { get; set; }
+
+        public string Email { get; set; }
+    }
+}
