78555d25f228217e7ee32910ff421acf288f89ee
Jesse Tabak <jessejunior@hotmail.nl>
2019-02-26 09:38:11 +0100

    nieuwsbrief and mail

--- surferdudes/surferdudes/Controllers/NieuwsbriefController.cs
+++ surferdudes/surferdudes/Controllers/NieuwsbriefController.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
+using surferdudes.Models;
+using System.Net.Mail;
+
 
 namespace surferdudes.Controllers
 {
     public class NieuwsbriefController : Controller
     {
+
+        // GET: Nieuwsbriefs
         public IActionResult Index()
         {
             return View();
+        }
+
+        // GET: Nieuwsbriefs/Details/5
+        
+
+        // GET: Nieuwsbriefs/Create
+        public IActionResult CreateNieuws()
+        {
+            return View();            
         }
     }
 }
---
+++ surferdudes/surferdudes/Models/Nieuwsbriefmodel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace surferdudes.Models
+{
+    public class Nieuwsbriefmodel
+    {
+        public int ID { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string Geslacht { get; set; }
+        public string Land { get; set; }
+        public string Email { get; set; }
+
+        public void SendMessage(String email, String subject, String body)
+        {
+            //SMTP server en poort 587
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.Credentials = new NetworkCredential("usermail", "userpassword");
+            smtpClient.EnableSsl = true;
+            //Gmail werkt met Securityprotocol TLS
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+            //smtpClient.UseDefaultCredentials = false;
+
+            MailMessage mail = new MailMessage();
+            mail.Body = "body";
+            mail.IsBodyHtml = true;
+            //Setting From , To and CC
+            mail.From = new MailAddress("email", "name");
+            mail.To.Add(new MailAddress("surfdudes2019@gmail.com"));
+
+            smtpClient.Send(mail);
+        }
+    }
+}
+
